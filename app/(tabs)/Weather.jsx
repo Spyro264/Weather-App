b327945de565a52000d03c9ef5cfed719e0f0570
Nagaraj_Vh <vhnagarajrakesh@gmail.com>
2025-08-29 18:41:10 +0530

    added splash screen created tab navigation

---
+++ app/(tabs)/Weather.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Weather = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Weather</Text>
+    </View>
+  );
+};
+
+export default Weather;
--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,19 +1,63 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 const _layout = () => {
   return (
-    <View
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "orange",
+        tabBarInactiveTintColor: "grey",
+
+        tabBarStyle: {
+          backgroundColor: "white",
+          height: 70,
+          paddingTop: 3,
+          borderRadius: 50,
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          overflow: "hidden",
+          margin: 10,
+          justifyContent: "center",
+          alignItems: "center",
+        },
+
+        tabBarIconStyle: {
+          marginTop: 5,
+        },
       }}
     >
-      <Text>_layout</Text>
-    </View>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Weather"
+        options={{
+          title: "Weather",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="cloud" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
 export default _layout;
-
-const styles = StyleSheet.create({});
