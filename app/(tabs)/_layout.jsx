c7fc155b0f39b339acbf3eda85cfd9b80adfe4e8
Nagaraj_Vh <vhnagarakrakesh.royalbrothers@gmail.com>
2025-08-29 15:39:33 +0530

    initial commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const _layout = () => {
+  return (
+    <View
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>_layout</Text>
+    </View>
+  );
+};
+
+export default _layout;
+
+const styles = StyleSheet.create({});
