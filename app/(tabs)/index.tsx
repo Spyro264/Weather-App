fe6079f2573f6f9e078499b2832e3344ea969587
Nagaraj_Vh <vhnagarajrakesh@gmail.com>
2025-08-30 22:47:15 +0530

    added some ui changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
-    // borderWidth: 1,
+    borderWidth: 1,
     borderColor: "red",
   },
   text: {
